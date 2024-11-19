28e905cd450b99869fbbf320773d64023307ed32
Rizki Darmawan <80609220+rizkidarmawan21@users.noreply.github.com>
2024-11-19 09:53:36 +0700

    feat: trust proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,7 +13,6 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies;
-    // protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
@@ -26,4 +25,9 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    public function __construct()
+    {
+        $this->proxies = env('TRUSTED_PROXIES', null);
+    }
 }
